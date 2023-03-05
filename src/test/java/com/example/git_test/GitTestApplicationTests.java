f4d01d1d3ba79dda1132ee360256d395178e5eef
hyz0247 <2472064879@qq.com>
2023-03-05 18:47:38 +0800

    初始化

---
+++ src/test/java/com/example/git_test/GitTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    public void test() {
+        System.out.println("abc......");
+    }
+
+}
